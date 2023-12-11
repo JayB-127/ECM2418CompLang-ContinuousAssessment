cea999519a02d9842983b8c64f815777e9650e2c
Jacob <jw.braidley@gmail.com>
2023-12-11 04:40:49 +0000

    check last elem
    + bug fix in toDigits

--- Audience.pl
+++ Audience.pl
@@ -1,10 +1,18 @@
 main :-
-    generator3(_).
+    generator3(X), tester3(X), write(X).
 
 % generates perfect squares from 1000 to 1000000
 generator3(X) :-
     between(1000, 1000000, X),
     square(X).
+
+% finds possible numbers from generated list
+tester3(X) :-
+    toDigits(X, XS),
+    unique(XS),
+    lastElem(XS, E),
+    listLength(XS, N),
+    E == N.
 
 % checks if a number is a perfect square
 square(X) :-
@@ -13,10 +21,10 @@ square(X) :-
 % converts int to list of digits
 toDigits(X, [X]) :-
     X < 10.
-toDigits(X, [XS]) :-
+toDigits(X, XS) :-
     X >= 10,
     div_mod(X, 10, D, M),
-    digits(D, R),
+    toDigits(D, R),
     append(R, [M], XS).
 
 % divides and mods two numbers
@@ -25,9 +33,9 @@ div_mod(A, B, D, M) :-
     M is A mod B.
 
 % checks a list of int is unique
-unique(XS) :-
-    sort(XS, S),
-    listLength(XS, Y),
+unique(X) :-
+    sort(X, S),
+    listLength(X, Y),
     listLength(S, Z),
     Y == Z.
 
@@ -36,6 +44,11 @@ listLength([], 0).
 listLength([_|XS], N) :-
     listLength(XS, N1),
     N is N1 + 1.
+
+% finds last element of list
+lastElem([X], X).
+lastElem([_|X], Y) :-
+    last(X, Y).
 
 % generator3 test func
 x_generator3(N) :-
